e2cf92445cc6495c1907d4882a15debafc662b9f
Peter Chen <pchen@4-south.com>
2013-09-24 15:32:50 -0500

    merge

--- public/dist/apptemplates.js
+++ public/dist/apptemplates.js
@@ -4,11 +4,11 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', hashTypes, hashContexts, escapeExpression=this.escapeExpression;
 
 
-  data.buffer.push("<div class=\"container\">\n  ");
+  data.buffer.push("<div class=\"container\">\r\n  ");
   hashTypes = {};
   hashContexts = {};
   data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "outlet", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push("\n</div>\n");
+  data.buffer.push("\r\n</div>\r\n");
   return buffer;
   
 });
@@ -19,7 +19,7 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashContexts, hashTypes, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
-  data.buffer.push("<h2>Let's log in!</h2>\n\n<form>\n  <fieldset>\n    <legend>Choose a name!</legend>\n    ");
+  data.buffer.push("<h2>Let's log in!</h2>\r\n\r\n<form>\r\n  <fieldset>\r\n    <legend>Choose a name!</legend>\r\n    ");
   hashContexts = {'type': depth0,'value': depth0};
   hashTypes = {'type': "ID",'value': "ID"};
   options = {hash:{
@@ -27,11 +27,11 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
     'value': ("potentialName")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   data.buffer.push(escapeExpression(((stack1 = helpers.input || depth0.input),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "input", options))));
-  data.buffer.push("\n    <div class=\"btn\" ");
+  data.buffer.push("\r\n    <div class=\"btn\" ");
   hashTypes = {};
   hashContexts = {};
   data.buffer.push(escapeExpression(helpers.action.call(depth0, "checkName", "potentialName", {hash:{},contexts:[depth0,depth0],types:["STRING","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push(">Select</div>\n  </fieldset>\n</form>\n");
+  data.buffer.push(">Select</div>\r\n  </fieldset>\r\n</form>\r\n");
   return buffer;
   
 });
